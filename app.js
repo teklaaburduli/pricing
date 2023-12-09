684eba37beb80c82306c5aef5ea0ad1b181ae76c
teklaaburduli <153001616+teklaaburduli@users.noreply.github.com>
2023-12-09 07:42:12 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// let burgerIcon = document.querySelector('.burgerIcon')
+// let linkContainer = document.querySelector('header .linkContainer')
+
+// burgerIcon.addEventListener('click', () => {
+//     if(linkContainer.style.display === 'none') {
+//         linkContainer.style.display = 'flex'
+//     } else {
+//         linkContainer.style.display = 'none'
+//     }
+// })
